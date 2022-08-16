3a9c8664e21a5fc8342c5645f5102c7be96471cd
Fabián Cortés <109831495+FabianSellside@users.noreply.github.com>
2022-08-16 16:40:09 -0400

    Create server.js
    Con esto se crea el servidor.

---
+++ server.js
@@ -0,0 +1,7 @@
+const https = require('http')
+const server = http.createServer((req, res) => {
+  console.log('New connection')
+  res.end('Hello Sellside')
+})
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => console.log('Listening'))
